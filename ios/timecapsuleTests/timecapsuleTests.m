2767d88f274f9230c6d7d5dbb1a983d9bb2b867f
Jose Arcidiacono <j2arcidiacono@outlook.com>
2023-02-09 07:53:40 -0300

    rename app

--- ios/timecapsuleTests/timecapsuleTests.m
+++ ios/timecapsuleTests/timecapsuleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FreqTrackerTests : XCTestCase
+@interface timecapsuleTests : XCTestCase
 
 @end
 
-@implementation FreqTrackerTests
+@implementation timecapsuleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
